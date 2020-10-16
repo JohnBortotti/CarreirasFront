ca5e46419ceaf885d6fe441ba727e96ee2f7ab69
John Bortotti <joao_bortotti@hotmail.com>
2020-10-16 16:59:56 -0300

    first commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,23 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router';
+
+import Home from './views/Home'
 
 Vue.config.productionTip = false
 
+Vue.use(VueRouter)
+
+const routes = [ // define as rotas e os componentes
+  { path: '/', component: Home, },
+];
+
+const router = new VueRouter({ // instancia o router
+  routes,
+  mode: 'history'
+});
+
 new Vue({
   render: h => h(App),
+  router,
 }).$mount('#app')
